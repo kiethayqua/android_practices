23d73880b854d5cb7d7d675c5d640892fdeaeba1
kietnguyen99 <kiet.nguyen2@mservice.com.vn>
2023-01-15 13:15:05 +0700

    refactor: using databinding

--- 2_DataBinding/app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
+++ 2_DataBinding/app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
@@ -1,24 +1,28 @@
 package com.anushka.bindingdemo2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ProgressBar
+import androidx.databinding.DataBindingUtil
+import com.anushka.bindingdemo2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button = findViewById(R.id.control_button)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        button = binding.controlButton
         button.setOnClickListener {
             startOrStopProgressBar()
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun startOrStopProgressBar() {
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val progressBar = binding.progressBar
         if (progressBar.visibility == View.GONE) {
             progressBar.visibility = View.VISIBLE
             button.text = "Stop"
